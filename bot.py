7f8f571f5d6f859d6f0e446ff53518a920bb8cc9
TylerJenningsW <tylerjennings2@outlook.com>
2022-05-30 21:56:43 -0500

    todo

--- bot.py
+++ bot.py
@@ -1,1 +1,21 @@
-test
+import nextcord
+import nextcord.ext.commands
+import os
+
+client = nextcord.Client()
+
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+# TODO
+# Command to add recipe
+
+# TODO
+# Get specific recipe
+
+# TODO
+# Get random recipe
+
+client.run(os.getenv('TOKEN'))
