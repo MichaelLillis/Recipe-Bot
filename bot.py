71d223ed0e808b7d7d6511ca4c3543c4d47b2cae
Michael Lillis <mikeplillis@gmail.com>
2022-05-30 23:01:59 -0400

    MAIN: Setup TODOs and infrastructure

--- bot.py
+++ bot.py
@@ -1,1 +1,21 @@
-test
+from nextcord.ext import commands
+
+# TODO: config functionality w/ prefix
+bot = commands.Bot(config["prefix"])
+print("Loading bot...")
+
+# TODO: Setup inital command with modal prompting users of recipe name/ingredients
+
+
+# TODO: Setup databse (SQL)
+
+
+# TODO: Take input of users and pass it to database
+
+
+# TODO: Setup server w/ bot
+# TODO: Setup token functionality w/ config
+try:
+    bot.run(config["token"])
+except Exception as e:
+    print(f"Error - Login Failed: {e}")
