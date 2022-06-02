542e1170d29ae38e865228e38a287196adf5880a
Michael Lillis <mikeplillis@gmail.com>
2022-06-01 22:42:36 -0400

    env fix

--- bot.py
+++ bot.py
@@ -2,7 +2,9 @@ import nextcord
 import nextcord.ext.commands
 import os
 import pyrebase
+from dotenv import load_dotenv
 from database import firebaseConfig
+
 client = nextcord.Client()
 
 
@@ -20,12 +22,14 @@ firebase = pyrebase.initialize_app(firebaseConfig)
 # TODO: Take input of users and pass it to database
 @client.slash_command
 async def add_recipe(ctx):
-
+    pass
     # TODO: Get specific recipe from database
 
     # TODO: Get random recipe from database
 
 try:
-    client.run(os.getenv('TOKEN'))
+    load_dotenv()
+    token = os.getenv("TOKEN")
+    client.run(token)
 except Exception as e:
     print(f"Error - Login Failed: {e}")
