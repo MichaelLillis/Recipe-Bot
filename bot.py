08481a9b21c113a1235583504d575fadaf2f8f26
TylerJenningsW <tylerjennings2@outlook.com>
2022-06-02 23:17:21 -0500

    MAIN: Data entry fix

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from discord import slash_command
 import nextcord
 import nextcord.ext.commands
 import os
@@ -17,7 +18,7 @@ async def on_ready():
 
 
 # TODO: Setup databse (FireBase)
-def new_recipe(recipe_name: str, ingredient_list: list[str], date: date, user: str):
+def new_recipe(recipe_name: str, ingredient_list: list[str], instructions: str, date: date, user: str):
     try:
         for ingredients in range(len(ingredient_list)):
             ingredient = ingredient_list[ingredients]
@@ -33,16 +34,19 @@ def new_recipe(recipe_name: str, ingredient_list: list[str], date: date, user: s
         data = {
             "Recipe": recipe_name,
             "Ingredients": ingredient_list,
-            "Date created": date, "Author": user
+            "Instructoons": instructions,
+            "Date created": date,
+            "Author": user
         }
-        db.child("Recipes").set(data)
+        db.child(f"{recipe_name} by {user}").set(data)
     except Exception as e:
         print(f"Error - Data Entry Add Failed: {e}")
 
 # TODO: Take input of users and pass it to database
 
 
-@client.slash_command
+@client.slash_command(name="Add recipe", description="Add a recipe to the recipe list", guild_ids=["SERVER"])
+@nextcord.ext.commands.cooldown(1, 60, type=nextcord.ext.commands.BucketType.default)
 async def add_recipe(ctx):
     pass
     # TODO: Get specific recipe from database
