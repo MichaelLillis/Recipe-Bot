35943114dffe484e9c005cef7bb7e96bc042e62d
Michael Lillis <36655595+MichaelLillis@users.noreply.github.com>
2022-06-18 03:24:55 -0400

    MAIN: Created database_example.py

---
+++ database_example.py
@@ -0,0 +1,12 @@
+import pyrebase
+
+firebaseConfig = {
+    "apiKey": "AIdfjkesidfjike382lksjfijl31289sfkd3js3",
+    "authDomain": "recipes-dx730.firebaseapp.com",
+    "databaseURL": "https://recipes-dx730-default-rtdb.firebaseio.com",
+    "projectId": "recipes-dx730",
+    "storageBucket": "recipes-dx730.appspot.com",
+    "messagingSenderId": "328348729503",
+    "appId": "1: 372837417818: web: j3382938isjdf8298sdfj9299",
+    "measurementId": "G-39JXDF32ZZ"
+}
