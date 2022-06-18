5eb483fa10a90f59de7eded7a5abf7cd1ac03a6b
TylerJenningsW <tylerjennings2@outlook.com>
2022-06-18 09:51:32 -0500

    MIN: Database example fix

--- database_example.py
+++ database_example.py
@@ -1,12 +1,36 @@
 import pyrebase
+from urllib.parse import quote
 
+# Temporary fix until this bug is fixed
+def patch():
+    from urllib.parse import quote
+    # Monkey patch pyrebase: replace quote function in pyrebase to workaround a bug.
+    # See https://github.com/thisbejim/Pyrebase/issues/294.
+    pyrebase.pyrebase.quote = lambda s, safe=None: s
+
+    # Monkey patch pyrebase: the Storage.get_url method does need quoting :|
+
+    def get_url(self, token=None):
+        path = self.path
+        self.path = None
+        if path.startswith('/'):
+            path = path[1:]
+        if token:
+            return "{0}/o/{1}?alt=media&token={2}".format(self.storage_bucket, quote(path, safe=''), token)
+        return "{0}/o/{1}?alt=media".format(self.storage_bucket, quote(path, safe=''))
+
+    pyrebase.pyrebase.Storage.get_url = lambda self, token=None: \
+        get_url(self, token)
+
+
+# Insert your config
 firebaseConfig = {
-    "apiKey": "AIdfjkesidfjike382lksjfijl31289sfkd3js3",
-    "authDomain": "recipes-dx730.firebaseapp.com",
-    "databaseURL": "https://recipes-dx730-default-rtdb.firebaseio.com",
-    "projectId": "recipes-dx730",
-    "storageBucket": "recipes-dx730.appspot.com",
-    "messagingSenderId": "328348729503",
-    "appId": "1: 372837417818: web: j3382938isjdf8298sdfj9299",
-    "measurementId": "G-39JXDF32ZZ"
+    "apiKey": "xxxxxxxxxxxxxxxxxx",
+    "authDomain": "xxxxxxxxxxxxxxxxxxxxxxxxxx",
+    "databaseURL": "https://xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    "projectId": "xxxxxxxxxxxxxxxxxxxx",
+    "storageBucket": "xxxxxxxxxxxxxxxxxxxxxxxx",
+    "messagingSenderId": "1111111111111111111",
+    "appId": "1: 11111111111111: web: xxxxxxxxxxxxxxxxxxxxxx",
+    "measurementId": "xxxxxxxxxxxxxxxxxxx"
 }
