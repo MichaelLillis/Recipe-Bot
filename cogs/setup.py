c1085a461429d163eb522f0938786282767beff1
TylerJenningsW <tylerjennings2@outlook.com>
2022-06-20 20:06:00 -0500

    Added perms

--- cogs/setup.py
+++ cogs/setup.py
@@ -1,3 +1,4 @@
+from discord import Permissions
 import nextcord
 from nextcord.ext import commands
 from src.setup_modal import SetupModal
@@ -11,6 +12,7 @@ class Recipe(commands.Cog):
     @nextcord.slash_command(
         description="Add a recipe to the recipes list!",
         guild_ids=[int(server)],
+        default_member_permissions=Permissions(administrator=True)
     )
     async def setup(self, interaction: nextcord.Interaction):
         await interaction.response.send_modal(SetupModal())
