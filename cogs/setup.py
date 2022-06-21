7a91a97bc756148e6e826e3c1091f505a6ad0a79
TylerJenningsW <tylerjennings2@outlook.com>
2022-06-20 21:33:08 -0500

    MAIN: Error fixes

--- cogs/setup.py
+++ cogs/setup.py
@@ -5,18 +5,17 @@ from src.setup_modal import SetupModal
 from main import server
 
 
-class Recipe(commands.Cog):
+class Setup(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @nextcord.slash_command(
         description="Setup the database config",
         guild_ids=[int(server)],
-        default_member_permissions=Permissions(administrator=True)
     )
     async def setup(self, interaction: nextcord.Interaction):
         await interaction.response.send_modal(SetupModal())
 
 
 def setup(bot):
-    bot.add_cog(Recipe(bot))
+    bot.add_cog(Setup(bot))
